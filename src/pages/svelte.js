c3a0bcee1ddd76df796cf7d7462db5b73864c8f9
Lauro Silva <hey@laurosilva.com>
2020-01-14 13:13:44 -0800

    Updating desing

--- src/pages/svelte.js
+++ src/pages/svelte.js
@@ -12,7 +12,19 @@ const Svelte = () => (
     </HeroContent>
 
     <Content>
-      <p>Let me know when it launches! </p>
+      <h2>Outline</h2>
+      <ul>
+        <li>Setting Up a First App</li>
+        <li>Base Syntax & Core Features</li>
+        <li>A Closer Look at Reactivity</li>
+        <li>Understanding the Different Component Types</li>
+        <li>Working with Bindings & Forms</li>
+        <li>Managing State & Data with Stores</li>
+        <li>Motion, Transitions & Animations</li>
+        <li>Network Interaction via Http</li>
+      </ul>
+      <h2>Out Feb, 2020</h2>
+      <p>Let me know when it launches! Sign up for the newsletter.👇🏽</p>
       <iframe
         title="Lauro's Newsletter"
         width="100%"
